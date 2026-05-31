dfce2d1dc9826746f61da6dac487fc6bbb651db9
awika2312-lab <awika.2312@gmail.com>
2026-05-31 23:17:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,9 @@
-import streamlit as st
-import pandas as pd
-import requests
-
-st.set_page_config(
-    page_title="Miu Stock Dashboard",
-    layout="wide"
-)
-
-st.title("📊 Miu Stock Dashboard")
-
-API_KEY = st.secrets["ALPHA_VANTAGE_KEY"]
-
-def get_overview(symbol):
-    url = (
-        f"https://www.alphavantage.co/query"
-        f"?function=OVERVIEW"
-        f"&symbol={symbol}"
-        f"&apikey={API_KEY}"
-    )
-    return requests.get(url).json()
-
-def get_quote(symbol):
-    url = (
-        f"https://www.alphavantage.co/query"
-        f"?function=GLOBAL_QUOTE"
-        f"&symbol={symbol}"
-        f"&apikey={API_KEY}"
-    )
-    return requests.get(url).json()
-
-symbols = ["NVDA", "RKLB"]
-
-rows = []
-
 for symbol in symbols:
 
     overview = get_overview(symbol)
     quote = get_quote(symbol)
 
-st.write(symbol, quote)
+    st.write(symbol, quote)
 
     quote_data = quote.get("Global Quote", {})
 
@@ -54,12 +19,3 @@ st.write(symbol, quote)
         "P/E": overview.get("PERatio", "N/A"),
         "P/S": overview.get("PriceToSalesRatioTTM", "N/A")
     })
-
-df = pd.DataFrame(rows)
-
-st.dataframe(
-    df,
-    use_container_width=True,
-    hide_index=True
-)
-
